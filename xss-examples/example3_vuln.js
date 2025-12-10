5260ac18e919bfbe55ba655b0d9486cce1e9b5ad
Luke Baja-Ricketts <57724155+github-username-number-2@users.noreply.github.com>
2025-12-09 20:46:58 -0500

    Update example3_vuln.js

--- xss-examples/example3_vuln.js
+++ xss-examples/example3_vuln.js
@@ -1,4 +1,6 @@
+import escapeHtml from "escape-html";
+
 app.get('/search', (req, res) => {
   const q = req.query.q || '';
-  res.send(`<h1>Results for ${q}</h1>`);
+  res.send(`<h1>Results for ${escapeHtml(q)}</h1>`);
 });
